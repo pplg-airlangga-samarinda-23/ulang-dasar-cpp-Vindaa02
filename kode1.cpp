588350faf414f45f2747547f2021a47b3a450f31
Novinda Jayadinignrum <23_novindajayadiningrum@student.smkti.net>
2024-07-31 11:26:44 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+void cetakData(string nama, string nisn, string jurusan, int tinggiBadan){
+    cout << "nama anda: " << nama << endl;
+    cout << "nisn anda: " << nisn << endl;
+    cout << "jurusan anda: " << jurusan << endl;
+    cout << "tinggi badan: " << tinggiBadan << endl;
 }
+
+int main() {
+    string nama = "Novinda Jayadiningrum";
+    string nisn = "0072811075";
+    string jurusan = "PPLG";
+    int tinggiBadan = 162;
+
+    cetakData(nama, nisn, jurusan, tinggiBadan);
+    return 0;
+} 
